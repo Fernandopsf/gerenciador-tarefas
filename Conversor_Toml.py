0db5008368d242dbce7df18eeb6a147e64fbcf13
Fernandopsf <fernandofilho27@terra.com.br>
2022-05-19 15:05:10 -0300

    tentativa 4 de upar

--- Conversor_Toml.py
+++ Conversor_Toml.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 18 22:41:21 2022
 
-@author: prumo
-"""
 
 import toml
 
@@ -13,19 +7,3 @@ data = toml.load("pyproject.toml.txt")
 f = open("pyproject.toml", "w")
 toml.dump(data, f)
 f.close()
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 18 22:41:21 2022
-
-@author: prumo
-"""
-
-import toml
-
-data = toml.load("pyproject.toml.txt")
-
-f = open("pyproject.toml", "w")
-toml.dump(data, f)
-f.close()
->>>>>>> 9eade0cc3adcbe53f013dc223292e6100cb81cc3
